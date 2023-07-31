b4b46967a697e9abf9b98ca02ff9f8fe2f313f60
Harshit S <harshitks2203@gmail.com>
2023-07-31 19:51:07 -0400

    clerk auth

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+const DashboardPage = () => {
+  return (
+    <div className="">
+      <p className="text-4xl text-green-700">Dashboard Page (protected)</p>
+    </div>
+  );
+};
+
+export default DashboardPage;
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <div className="">
-      <p className="text-4xl text-green-700">Hello Visionary AI</p>
-    </div>
-  );
-}
