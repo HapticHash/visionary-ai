e8b69e7484a2cda44713575d7937712c516f9a65
Harshit S <harshitks2203@gmail.com>
2023-07-31 19:19:11 -0400

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="">
+      <p className="text-4xl text-green-700">Hello Visionary AI</p>
+    </div>
+  );
+}
