16b262cfb4ed35124051731a1debb7816b715ffc
Harshit S <harshitks2203@gmail.com>
2023-08-07 16:45:10 -0500

    removed generation limit if pro

--- app/(dashboard)/(routes)/settings/page.tsx
+++ app/(dashboard)/(routes)/settings/page.tsx
@@ -1,5 +1,29 @@
-const SettingPage = () => {
-  return <div>Setting</div>;
-};
+import { Settings } from "lucide-react";
 
+import { checkSubscription } from "@/lib/subscription";
+import { Heading } from "@/components/Heading";
+import { SubscriptionButton } from "@/components/Subscription-Button";
+
+const SettingPage = async () => {
+  const isPro = await checkSubscription();
+  return (
+    <div>
+      <Heading
+        title="Settings"
+        description="Manage account settings."
+        icon={Settings}
+        iconColor="text-gray-700"
+        bgColor="bg-gray-700/10"
+      />
+      <div className="px-4 lg:px-8 space-y-4">
+        <div className="text-muted-foreground text-sm">
+          {isPro
+            ? "You are currently on a Pro plan."
+            : "You are currently on a free plan."}
+        </div>
+        <SubscriptionButton isPro={isPro} />
+      </div>
+    </div>
+  );
+};
 export default SettingPage;
--- components/FreeCounter.tsx
+++ components/FreeCounter.tsx
@@ -10,9 +10,13 @@ import { useProModal } from "@/hooks/use-pro-modal";
 
 interface FreeCounterProps {
   apiLimitCount: number;
+  isPro: boolean;
 }
 
-export const FreeCounter = ({ apiLimitCount }: FreeCounterProps) => {
+export const FreeCounter = ({
+  apiLimitCount = 0,
+  isPro = false,
+}: FreeCounterProps) => {
   const proModal = useProModal();
   const [mounted, setMounted] = useState(false);
 
@@ -21,6 +25,10 @@ export const FreeCounter = ({ apiLimitCount }: FreeCounterProps) => {
   }, []);
 
   if (!mounted) {
+    return null;
+  }
+
+  if (isPro) {
     return null;
   }
 
